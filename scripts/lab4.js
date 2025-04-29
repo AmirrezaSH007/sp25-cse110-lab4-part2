4a7d2d101818d93e0e058783539fb5fe2598bc86
AmirrezaSH007 <165849887+AmirrezaSH007@users.noreply.github.com>
2025-04-29 01:26:48 -0700

    Update lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,20 +6,15 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
+
+    
+    if (!add) {
+        return false:
+    }
     if (typeof num1 !== 'number' || typeof num2 !== 'number') {
         return false;
     }
-    
-    if (add) {
-        let result = 0;
-
         result = num1 + num2;
-
-        return result;
-    }
-    else {
-        return !add;
-    }
 }
 
 /**
